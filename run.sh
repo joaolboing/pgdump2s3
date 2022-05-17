6b27d5307f1363a0d2494549c2cb35d16910f952
João Boing <joao@conectra.com.br>
2022-05-17 08:18:50 -0300

    feat: update s3 folder structure

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ DATE=`date +%Y-%m-%d_%H-%M`
 
 YEAR=`date +%Y`
 MONTH=`date +%m`
+DAY=`date +%d`
 
 BKPNAME=${DB_NAME}-${DATE}
 
@@ -15,4 +16,4 @@ echo $(date) "Backup iniciado"
 pg_dump -h ${DB_HOST} -p ${DB_PORT} -U${DB_USER} -Fp -c -C -O ${DB_NAME} | gzip > /tmp/${BKPNAME}.sql.gz
 
 echo $(date) "Copiando arquivo para o bucket s3"
-aws s3 cp /tmp/${BKPNAME}.sql.gz s3://${BUCKET_NAME}/${BKP_PREFIX}${DB_NAME}/${YEAR}/${MONTH}/${BKPNAME}.sql.gz --region ${AWS_REGION}
+aws s3 cp /tmp/${BKPNAME}.sql.gz s3://${BUCKET_NAME}/${YEAR}/${MONTH}/${DAY}/${BKPNAME}.sql.gz --region ${AWS_REGION}
